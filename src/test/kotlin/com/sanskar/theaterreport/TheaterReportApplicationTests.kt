b25ad416357ff39e26bc95d4ff8d2a3d0017adb1
Sanskar Deepak <sanskar.verma90@gmail.com>
2023-01-18 12:32:06 +1100

    first commit

---
+++ src/test/kotlin/com/sanskar/theaterreport/TheaterReportApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sanskar.theaterreport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheaterReportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
