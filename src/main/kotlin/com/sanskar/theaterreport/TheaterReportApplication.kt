b9171c4ad357877af5d838c385613f6b8e7ac0d9
Sanskar Deepak <sanskar.verma90@gmail.com>
2023-01-23 17:55:48 +1100

    Simplify configuration scanning

--- src/main/kotlin/com/sanskar/theaterreport/TheaterReportApplication.kt
+++ src/main/kotlin/com/sanskar/theaterreport/TheaterReportApplication.kt
@@ -1,12 +1,11 @@
 package com.sanskar.theaterreport
 
-import com.sanskar.theaterreport.config.AWSCloudProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(AWSCloudProperties::class)
+@ConfigurationPropertiesScan
 class TheaterReportApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/sanskar/theaterreport/config/AWSCloudProperties.kt
+++ src/main/kotlin/com/sanskar/theaterreport/config/AWSCloudProperties.kt
@@ -1,9 +1,7 @@
 package com.sanskar.theaterreport.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 
 @ConfigurationProperties(prefix = "cloud.aws")
-@ConfigurationPropertiesScan
 data class AWSCloudProperties(val region: String,
                               val reportS3Bucket: String)
