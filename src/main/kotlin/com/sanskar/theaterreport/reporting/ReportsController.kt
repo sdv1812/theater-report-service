f9911c3d5df82c14a4d5e83724322bcb3c023ef7
Sanskar Deepak <sanskar.verma90@gmail.com>
2023-01-23 17:17:58 +1100

    Use properties to set bucket name and region for s3 upload

--- src/main/kotlin/com/sanskar/theaterreport/reporting/ReportsController.kt
+++ src/main/kotlin/com/sanskar/theaterreport/reporting/ReportsController.kt
@@ -3,24 +3,27 @@ package com.sanskar.theaterreport.reporting
 import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.model.PutObjectRequest
 import aws.smithy.kotlin.runtime.content.ByteStream
+import com.sanskar.theaterreport.config.AWSCloudProperties
 import kotlinx.coroutines.runBlocking
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 import kotlin.reflect.full.declaredMemberFunctions
 
 @RestController
 @RequestMapping("/reports")
-class ReportsController {
+class ReportsController(
+    private var reportingService: ReportsService,
+    private var awsCloudProperties: AWSCloudProperties
+) {
 
-
-
-    @Autowired
-    lateinit var reportingService: ReportsService
     private fun getListOfReports() = reportingService::class.declaredMemberFunctions.map { it.name }
 
     @RequestMapping("")
@@ -29,26 +32,26 @@ class ReportsController {
     @RequestMapping("/generateReport/{report}", produces = [MediaType.TEXT_HTML_VALUE])
     fun getReport(@PathVariable report: String) {
         val matchedReport = reportingService::class.declaredMemberFunctions.firstOrNull { it.name == report }
-        val result = matchedReport?.call(reportingService).toString()
+        val rawResult = matchedReport?.call(reportingService) ?: return
         runBlocking {
-            S3Client.fromEnvironment { region = REGION }
+            S3Client.fromEnvironment { region = awsCloudProperties.region }
                 .use { s3 ->
-
+                    val keyStr = "${matchedReport.name}-${ZonedDateTime.now(
+                        ZoneId.of("Australia/Sydney")
+                    ).format(
+                        DateTimeFormatter
+                            .ofLocalizedDateTime(FormatStyle.LONG)
+                            .withLocale( Locale.ENGLISH )
+                    ) }"
                     s3.putObject(PutObjectRequest {
-                        bucket = BUCKET
-                        key = KEY
-                        body = ByteStream.fromString(result)
+                        bucket = awsCloudProperties.reportS3Bucket
+                        key = keyStr
+                        body = ByteStream.fromString(rawResult.toString())
                     })
 
-                    println("Object ${BUCKET}/$KEY created successfully!")
+                    println("Object ${awsCloudProperties.reportS3Bucket}/$keyStr created successfully!")
 
                 }
         }
-    }
-
-    companion object {
-        const val REGION = "us-west-2"
-        val KEY = "key-${UUID.randomUUID()}"
-        const val BUCKET = "bucket-sanskar"
     }
 }
